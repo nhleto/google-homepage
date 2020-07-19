c3818ff01a1e2791f1abfe916ab60a86c89b0304
Henry Leto <hnr.leto11@gmail.com>
2020-07-19 19:41:37 -0400

    commits
    commits

---
+++ the_odin_project/problems.js
@@ -0,0 +1,106 @@
+/*
+switch (choice){
+    case 'white':
+        bgColor='white'
+        textColor='black'
+        update()
+        break;
+    case 'black':
+        bgColor='black'
+        textColor='white'
+        update()
+        break;
+    case 'purple':
+        bgColor='purple'
+        textColor='yellow'
+        update()
+        break;
+    case 'yellow':
+        bgColor='yellow'
+        textColor='purple'
+        update()
+        break;
+    case 'psychadelic':
+        bgColor='psychadelic'
+        textColor='black'
+        update()
+        break;
+}
+
+
+if (season=='summer'){
+    response=('its sunny out')
+}
+if (season==null){
+    response=('we dont know')
+}
+
+if (machineActive==false) {
+    response=('turned off')
+    }else{
+    response=('turned on')
+    }
+if (machineActive==true){
+    (score<0 >100)
+    response=('This is not possible, an error has occurred')
+}else{
+    (score<=19)
+    response=('This is a terrible score')
+}if  (score>=30<40){
+    response=('You know some things but its a prett bad score')
+}else{
+    (score>=70<90)
+    response=('That\'s a great score, you really know your stuff.')
+}
+
+
+
+
+
+
+if (machineActive==false) {
+    machineResult=('turned off')
+    }else{
+    machineResult=('turned on')
+    } if (machineResult=true){
+        ('enter pwd')
+        pwd==prompt()
+        if (pwd=='cheese'){
+            pwdResult=('logon successfull')
+        }else{ 
+            pwd!='cheese'
+            pwdResult=='FAILURE'
+            }
+        }
+
+((machineActive==false) && (machineResult='off')) || (machineActive=true)(machineResults='turned on')&&(pwd='cheese')(pwdResult='logon successfull')||(pwd!='cheese')(pwdResult='logonFailure')
+
+
+((machineActive=false) && (machineResult='off')) ||(machineResults='turned on')&&(pwd='cheese')(pwdResult='logon successfull')||(pwd!=='cheese')(pwdResult='logonFailure')
+
+answer= ''
+prompt('what is the officla name of jave script')
+if (answer='ecmascript'){
+    alert('righh')
+} else {
+    answer!=('ECMA')
+    alert('Didnt know? ECMA script!')
+}
+*/
+const countdown=[1,2,3,4,5,6,7,8,9,10]
+
+for (i=0;i<countdown.length;i++){
+    console.log(String(i))
+}if (i===countdown.length -1) {
+        console.log('blastoff!')
+    }
+
+
+
+
+
+
+
+
+
+
